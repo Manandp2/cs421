eb1cf7c4b42aea29e7b7894e480b634177a23167
Manandp2 <manandp2@illinois.edu>
2026-02-22 19:09:20 -0600

    Add mp1 code

--- mp1a-recursion/src/Lib.hs
+++ mp1a-recursion/src/Lib.hs
@@ -7,13 +7,21 @@
 module Lib where
 
 -- This line imports the Prelude module without certain functions
-import Prelude hiding ( take, drop, reverse
-                      , zip, zipWith
-                      , map, foldl, foldr
-                      , iterate, repeat
-                      , replicate, cycle
-                      , (++)
-                      )
+import Prelude hiding
+  ( cycle,
+    drop,
+    foldl,
+    foldr,
+    iterate,
+    map,
+    repeat,
+    replicate,
+    reverse,
+    take,
+    zip,
+    zipWith,
+    (++),
+  )
 -- When you are allowed to use builtin functions Prepend them with "P."
 -- for example `P.take`
 import qualified Prelude as P
@@ -21,7 +29,9 @@ import qualified Prelude as P
 --- Metadata for autograder
 --- -----------------------
 tag1 = 21923
+
 tag2 = 44437
+
 tag3 = 24929
 
 --- Problems
@@ -33,57 +43,90 @@ tag3 = 24929
 --- ### mytake
 
 -- don't forget to put the type declaration or you will lose points!
-mytake = undefined
+mytake :: Int -> [a] -> [a]
+mytake _ [] = []
+mytake n (x : xs)
+  | n > 0 = x : mytake (n - 1) xs
+  | otherwise = []
 
 --- ### mydrop
 
 -- don't forget to put the type declaration or you will lose points!
-mydrop = undefined
+mydrop :: Int -> [a] -> [a]
+mydrop _ [] = []
+mydrop n l@(x : xs)
+  | n > 0 = mydrop (n - 1) xs
+  | otherwise = l
 
 --- ### rev
 
 -- don't forget to put the type declaration or you will lose points!
-rev = undefined
+rev :: [a] -> [a]
+rev xx = revHelp xx []
+  where
+    revHelp [] a = a
+    revHelp (x : xs) a = revHelp xs (x : a)
 
 --- ### app
 
 -- don't forget to put the type declaration or you will lose points!
-app = undefined
+app :: [a] -> [a] -> [a]
+app [] yy = yy
+app (x : xs) yy = x : app xs yy
 
 --- ### inclist
 
 -- don't forget to put the type declaration or you will lose points!
-inclist = undefined
+
+inclist :: (Num a) => [a] -> [a]
+inclist [] = []
+inclist (x : xs) = (x + 1) : inclist xs
 
 --- ### sumlist
 
 -- don't forget to put the type declaration or you will lose points!
-sumlist = undefined
+sumlist :: (Num a) => [a] -> a
+sumlist xx = summer xx 0
+  where
+    summer [] acc = acc
+    summer (x : xs) acc = summer xs $ acc + x
 
 --- ### myzip
 
 -- don't forget to put the type declaration or you will lose points!
-myzip = undefined
+myzip :: [a] -> [b] -> [(a, b)]
+myzip [] _ = []
+myzip _ [] = []
+myzip (x : xs) (y : ys) = (x, y) : myzip xs ys
 
 --- ### addpairs
 
 -- don't forget to put the type declaration or you will lose points!
-addpairs = undefined
+addpairs :: (Num a) => [a] -> [a] -> [a]
+addpairs xx yy = zipSum (myzip xx yy)
+  where
+    zipSum [] = []
+    zipSum ((x, y) : ls) = (x + y) : zipSum ls
 
 --- ### ones
 
 -- don't forget to put the type declaration or you will lose points!
-ones = undefined
+ones :: [Integer]
+ones = 1 : ones
 
 --- ### nats
 
 -- don't forget to put the type declaration or you will lose points!
-nats = undefined
+nats :: [Integer]
+nats = nh 0
+  where
+    nh a = a : nh (a + 1)
 
 --- ### fib
 
 -- don't forget to put the type declaration or you will lose points!
-fib = undefined
+fib :: [Integer]
+fib = 0 : 1 : addpairs fib (tail fib)
 
 --- Set Theory
 --- ----------
@@ -91,22 +134,45 @@ fib = undefined
 --- ### add
 
 -- don't forget to put the type declaration or you will lose points!
-add = undefined
+add :: (Ord a) => a -> [a] -> [a]
+add n [] = [n]
+add n s@(x : xs)
+  | n > x = x : add n xs
+  | n < x = n : s
+  | otherwise = s
 
 --- ### union
 
 -- don't forget to put the type declaration or you will lose points!
-union = undefined
+
+union :: (Ord a) => [a] -> [a] -> [a]
+union [] yy = yy
+union xx [] = xx
+union xx@(x : xs) yy@(y : ys)
+  | x < y = x : union xs yy
+  | y < x = y : union xx ys
+  | x == y = x : union xs ys
 
 --- ### intersect
 
 -- don't forget to put the type declaration or you will lose points!
-intersect = undefined
+
+intersect :: (Ord a) => [a] -> [a] -> [a]
+intersect [] _ = []
+intersect _ [] = []
+intersect xx@(x : xs) yy@(y : ys)
+  | x == y = x : intersect xs ys
+  | x < y = intersect xs yy
+  | x > y = intersect xx ys
 
 --- ### powerset
 
 -- don't forget to put the type declaration or you will lose points!
-powerset = undefined
+powerset :: (Ord a) => [a] -> [[a]]
+powerset [] = [[]]
+powerset (x : xs) = union baseSet $ P.map (add x) baseSet
+  where
+    baseSet = powerset xs
 
 --- Higher Order Functions
 --- ----------------------
@@ -114,9 +180,11 @@ powerset = undefined
 --- ### inclist'
 
 -- don't forget to put the type declaration or you will lose points!
-inclist' = undefined
+inclist' :: (Num a) => [a] -> [a]
+inclist' = P.map (+ 1)
 
 --- ### sumlist'
 
 -- don't forget to put the type declaration or you will lose points!
-sumlist' = undefined
+sumlist' :: (Num a) => [a] -> a
+sumlist' = P.foldr (+) 0
--- mp1b-adts/src/Lib.hs
+++ mp1b-adts/src/Lib.hs
@@ -9,7 +9,9 @@ module Lib where
 --- Metadata for autograder
 --- -----------------------
 tag1 = 21923
+
 tag2 = 44437
+
 tag3 = 24929
 
 --- Problems
@@ -18,49 +20,73 @@ tag3 = 24929
 --- Algebraic Data Types
 --- --------------------
 
-data List a = Cons a (List a)
-            | Nil
+data List a
+  = Cons a (List a)
+  | Nil
   deriving (Show, Eq)
 
-data Exp = IntExp Integer
-         | PlusExp [Exp]
-         | MultExp [Exp]
+data Exp
+  = IntExp Integer
+  | PlusExp [Exp]
+  | MultExp [Exp]
   deriving (Show, Eq)
 
 --- ### list2cons
 
 -- don't forget to put the type declaration or you will lose points!
-list2cons = undefined
+list2cons :: [a] -> List a
+list2cons [] = Nil
+list2cons (x : xs) = Cons x (list2cons xs)
 
 --- ### cons2list
 
 -- don't forget to put the type declaration or you will lose points!
-cons2list = undefined
+cons2list :: List a -> [a]
+cons2list Nil = []
+cons2list (Cons x xs) = x : cons2list xs
 
 --- ### eval
 
 -- don't forget to put the type declaration or you will lose points!
-eval = undefined
+eval :: Exp -> Integer
+eval (IntExp x) = x
+eval (PlusExp xx) = sum $ map eval xx
+eval (MultExp xx) = product $ map eval xx
 
 --- ### list2cons'
 
 -- don't forget to put the type declaration or you will lose points!
-list2cons' = undefined
+list2cons' :: [a] -> List a
+list2cons' = foldr Cons Nil
 
 --- ### BinTree
 
 -- BinTree
+data BinTree a
+  = Node a (BinTree a) (BinTree a)
+  | Leaf
+  deriving (Show)
 
 --- ### sumTree
 
 -- don't forget to put the type declaration or you will lose points!
-sumTree = undefined
+sumTree :: (Num a) => BinTree a -> a
+sumTree Leaf = 0
+sumTree (Node x l r) = x + sumTree l + sumTree r
 
 --- ### SimpVal
 
 -- SimpVal
+data SimpVal
+  = IntVal Integer
+  | BoolVal Bool
+  | StrVal String
+  | ExnVal String
+  deriving (Show)
 
 --- ### liftIntOp
 
 -- don't forget to put the type declaration or you will lose points!
-liftIntOp = undefined
+liftIntOp :: (Integer -> Integer -> Integer) -> SimpVal -> SimpVal -> SimpVal
+liftIntOp f (IntVal a) (IntVal b) = IntVal $ f a b
+liftIntOp _ _ _ = ExnVal "not an IntVal!"
