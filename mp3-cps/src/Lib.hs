28b23c7f93cb8bb88e79106b889c0c9b7e030edb
Manandp2 <manandp2@illinois.edu>
2026-03-19 11:40:06 -0500

    implemented first 2

--- mp3-cps/src/Lib.hs
+++ mp3-cps/src/Lib.hs
@@ -3,57 +3,74 @@
 
 module Lib where
 
-import System.IO (hPutStrLn, hPutStr, stdout, hFlush)
-
-import Data.List (intercalate)
-
 import Data.Functor.Identity (Identity)
-import Text.ParserCombinators.Parsec hiding (Parser)
+import Data.List (intercalate)
+import System.IO (hFlush, hPutStr, hPutStrLn, stdout)
 import Text.Parsec.Prim (ParsecT)
+import Text.ParserCombinators.Parsec hiding (Parser)
 
 --- Metadata for autograder
 --- -----------------------
 tag1 = 36392
+
 tag2 = 13977
+
 tag3 = 68529
 
 --- The Types
 --- ---------
 
 data Stmt = Decl String [String] Exp
-            deriving (Eq)
+  deriving (Eq)
 
 instance Show Stmt where
-    show (Decl f params exp) = f ++ " " ++ intercalate " " params ++ " = " ++ (show exp)
+  show (Decl f params exp) = f ++ " " ++ intercalate " " params ++ " = " ++ (show exp)
 
-data Exp = IntExp Integer
-         | VarExp String
-         | LamExp String Exp
-         | IfExp Exp Exp Exp
-         | OpExp String Exp Exp
-         | AppExp Exp Exp
-         deriving (Eq)
+data Exp
+  = IntExp Integer
+  | VarExp String
+  | LamExp String Exp
+  | IfExp Exp Exp Exp
+  | OpExp String Exp Exp
+  | AppExp Exp Exp
+  deriving (Eq)
 
 instance Show Exp where
-    show (VarExp s)       = s
-    show (IntExp i)       = show i
-    show (LamExp x e)     = "(\\" ++ x ++ " -> " ++ (show e) ++ ")"
-    show (IfExp e1 e2 e3) = "(if " ++ show e1 ++ " then " ++ show e2
-                            ++ " else " ++ show e3 ++ ")"
-    show (OpExp op e1 e2) = "(" ++ show e1 ++ " " ++ op ++ " " ++ show e2 ++ ")"
-    show (AppExp f e)     = show f ++ " " ++ show e
+  show (VarExp s) = s
+  show (IntExp i) = show i
+  show (LamExp x e) = "(\\" ++ x ++ " -> " ++ (show e) ++ ")"
+  show (IfExp e1 e2 e3) =
+    "(if "
+      ++ show e1
+      ++ " then "
+      ++ show e2
+      ++ " else "
+      ++ show e3
+      ++ ")"
+  show (OpExp op e1 e2) = "(" ++ show e1 ++ " " ++ op ++ " " ++ show e2 ++ ")"
+  show (AppExp f e) = show f ++ " " ++ show e
 
 ctorShow :: Exp -> String
-ctorShow (VarExp s)       = "VarExp " ++ show s
-ctorShow (IntExp i)       = "IntExp " ++ show i
-ctorShow (LamExp x e)     = "LamExp " ++ show x ++ " (" ++ ctorShow e ++ ")"
-ctorShow (IfExp e1 e2 e3) = "IfExp (" ++ ctorShow e1 ++ ") ("
-                                ++ ctorShow e2 ++ ") ("
-                                ++ ctorShow e3 ++ ")"
-ctorShow (OpExp op e1 e2) = "OpExp " ++ show op ++ " ("
-                                ++ ctorShow e1 ++ ") ("
-                                ++ ctorShow e2 ++ ")"
-ctorShow (AppExp f e)     = "AppExp (" ++ ctorShow f ++ ") (" ++ ctorShow e ++ ")"
+ctorShow (VarExp s) = "VarExp " ++ show s
+ctorShow (IntExp i) = "IntExp " ++ show i
+ctorShow (LamExp x e) = "LamExp " ++ show x ++ " (" ++ ctorShow e ++ ")"
+ctorShow (IfExp e1 e2 e3) =
+  "IfExp ("
+    ++ ctorShow e1
+    ++ ") ("
+    ++ ctorShow e2
+    ++ ") ("
+    ++ ctorShow e3
+    ++ ")"
+ctorShow (OpExp op e1 e2) =
+  "OpExp "
+    ++ show op
+    ++ " ("
+    ++ ctorShow e1
+    ++ ") ("
+    ++ ctorShow e2
+    ++ ")"
+ctorShow (AppExp f e) = "AppExp (" ++ ctorShow f ++ ") (" ++ ctorShow e ++ ")"
 
 --- Problems
 --- ========
@@ -64,12 +81,18 @@ ctorShow (AppExp f e)     = "AppExp (" ++ ctorShow f ++ ") (" ++ ctorShow e ++ "
 --- ### `factk :: Integer -> (Integer -> t) -> t`
 
 factk :: Integer -> (Integer -> t) -> t
-factk = undefined
+factk 0 k = k 1
+factk n k = factk (n - 1) (\v -> k (n * v))
 
 --- ### `evenoddk :: [Integer] -> (Integer -> t) -> (Integer -> t) -> t`
 
 evenoddk :: [Integer] -> (Integer -> t) -> (Integer -> t) -> t
-evenoddk = undefined
+evenoddk [x] ek ok
+  | even x = ek x
+  | otherwise = ok x
+evenoddk (x : xs) ek ok
+  | even x = evenoddk xs (\v -> ek (x + v)) ok
+  | otherwise = evenoddk xs ek (\v -> ok (x + v))
 
 --- Automated Translation
 --- ---------------------
