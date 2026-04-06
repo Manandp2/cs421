94b38da6d0f0cf0c808b62bebe5249cc5cb0e00d
Manandp2 <manandp2@illinois.edu>
2026-04-06 14:40:32 -0500

    mp4

--- mp4-forth/src/Lib.hs
+++ mp4-forth/src/Lib.hs
@@ -4,6 +4,7 @@
 module Lib where
 
 type ErrorMsg = String
+
 -- for stack underflow errors
 msgUnderflow :: ErrorMsg
 msgUnderflow = "Stack underflow"
@@ -31,30 +32,37 @@ underflow = error msgUnderflow
 --- ---------
 
 type ForthState = (IStack, Dictionary, Output)
+
 type Transition = (ForthState -> ForthState)
-type IStack     = [Integer]
+
+type IStack = [Integer]
+
 type Dictionary = [(String, Value)]
-type Output     = [String]
 
-type CStack     = [(String, Transition)]
+type Output = [String]
 
-data Value = Prim Transition
-           | Define
-           | EndDef
-           | Compile (CStack -> Maybe CStack)
-           | Num Integer
-           | Unknown String
+type CStack = [(String, Transition)]
+
+data Value
+  = Prim Transition
+  | Define
+  | EndDef
+  | Compile (CStack -> Maybe CStack)
+  | Num Integer
+  | Unknown String
 
 instance Show Value where
-    show (Prim f)    = "Prim"
-    show (Compile _) = msgCompileOnly
-    show Define      = "Define"
-    show EndDef      = msgCompileOnly
-    show (Num i)     = show i
-    show (Unknown s) = msgUndefinedSym s
+  show (Prim f) = "Prim"
+  show (Compile _) = msgCompileOnly
+  show Define = "Define"
+  show EndDef = msgCompileOnly
+  show (Num i) = show i
+  show (Unknown s) = msgUndefinedSym s
 
 tag1 = 47182
+
 tag2 = 83594
+
 tag3 = 26037
 
 --- Dictionary Access
@@ -64,18 +72,18 @@ tag3 = 26037
 
 -- handle input lookups (and integers)
 dlookup :: String -> Dictionary -> Value
-dlookup word dict
-    = case lookup word dict of
-        Just x -> x
-        _          -> case reads word of
-                        [(i,"")] -> Num i
-                        _        -> Unknown word
+dlookup word dict =
+  case lookup word dict of
+    Just x -> x
+    _ -> case reads word of
+      [(i, "")] -> Num i
+      _ -> Unknown word
 
 --- ### Insert
 
 -- handle inserting things into the dictionary
 dinsert :: String -> Value -> Dictionary -> Dictionary
-dinsert key val dict = (key, val):dict
+dinsert key val dict = (key, val) : dict
 
 --- Problems
 --- ========
@@ -84,155 +92,199 @@ dinsert key val dict = (key, val):dict
 --- -------
 
 liftIStackOp :: (IStack -> Maybe IStack) -> ForthState -> ForthState
-liftIStackOp op (i, d, o)
-    = case op i of
-        Just i' -> (i', d, o)
-        Nothing -> underflow
+liftIStackOp op (i, d, o) =
+  case op i of
+    Just i' -> (i', d, o)
+    Nothing -> underflow
 
 --- ### `liftIntOp`
 
 liftIntOp :: (Integer -> Integer -> Integer) -> IStack -> Maybe IStack
-liftIntOp op (x:y:xs) = Just $ (y `op` x) : xs
-liftIntOp _  _        = Nothing
+liftIntOp op (x : y : xs) = Just $ (y `op` x) : xs
+liftIntOp _ _ = Nothing
 
 --- ### `liftCompOp`
 
 liftCompOp :: (Integer -> Integer -> Bool) -> IStack -> Maybe IStack
-liftCompOp = undefined
-
+liftCompOp op (x : y : xs) = Just $ (if y `op` x then -1 else 0) : xs
+liftCompOp _ _ = Nothing
 
 --- The Dictionary
 --- --------------
 
 initialDictionary :: Dictionary
-initialDictionary = initArith ++ initComp ++ initIStackOp ++ initPrintOp
-                 ++ initCompileOp
+initialDictionary =
+  initArith
+    ++ initComp
+    ++ initIStackOp
+    ++ initPrintOp
+    ++ initCompileOp
 
-initCompileOp = [ (":",    Define)
-                , (";",    EndDef)
-                , ("for",  Compile cstackFor)
-                , ("next", Compile cstackNext)
-                , ("if",   Compile cstackIf)
-                , ("else", Compile cstackElse)
-                , ("then", Compile cstackThen)
-                , ("begin",Compile cstackBegin)
-                , ("until",Compile cstackUntil)
-                ]
+initCompileOp =
+  [ (":", Define),
+    (";", EndDef),
+    ("for", Compile cstackFor),
+    ("next", Compile cstackNext),
+    ("if", Compile cstackIf),
+    ("else", Compile cstackElse),
+    ("then", Compile cstackThen),
+    ("begin", Compile cstackBegin),
+    ("until", Compile cstackUntil)
+  ]
 
 --- ### Arithmetic Operators
 
 initArith :: Dictionary
-initArith = [ ("+",  Prim $ liftIStackOp $ liftIntOp (+))
-            ]
+initArith =
+  [ ("+", Prim $ liftIStackOp $ liftIntOp (+)),
+    ("-", Prim $ liftIStackOp $ liftIntOp (-)),
+    ("*", Prim $ liftIStackOp $ liftIntOp (*)),
+    ("/", Prim $ liftIStackOp $ liftIntOp div)
+  ]
 
 --- ### Comparison Operators
 
 initComp :: Dictionary
-initComp = []
+initComp =
+  [ ("<", Prim $ liftIStackOp $ liftCompOp (<)),
+    (">", Prim $ liftIStackOp $ liftCompOp (>)),
+    ("<=", Prim $ liftIStackOp $ liftCompOp (<=)),
+    (">=", Prim $ liftIStackOp $ liftCompOp (>=)),
+    ("=", Prim $ liftIStackOp $ liftCompOp (==)),
+    ("!=", Prim $ liftIStackOp $ liftCompOp (/=))
+  ]
 
 --- ### Stack Manipulations
 
 initIStackOp :: Dictionary
-initIStackOp = [ ("dup",  Prim $ liftIStackOp istackDup)
-               ]
+initIStackOp =
+  [ ("dup", Prim $ liftIStackOp istackDup),
+    ("swap", Prim $ liftIStackOp istackSwap),
+    ("drop", Prim $ liftIStackOp istackDrop),
+    ("rot", Prim $ liftIStackOp istackRot)
+  ]
 
-initPrintOp = [ (".",  Prim printPop)
-              ]
+initPrintOp =
+  [ (".", Prim printPop),
+    (".S", Prim printStack)
+  ]
 
 istackDup :: IStack -> Maybe IStack
-istackDup (i:is) = Just $ i:i:is
-istackDup _      = Nothing
+istackDup (i : is) = Just $ i : i : is
+istackDup _ = Nothing
 
 istackSwap :: IStack -> Maybe IStack
-istackSwap = undefined
+istackSwap (i1 : i2 : is) = Just (i2 : i1 : is)
+istackSwap _ = Nothing
 
 istackDrop :: IStack -> Maybe IStack
-istackDrop = undefined
+istackDrop (_ : is) = Just is
+istackDrop _ = Nothing
 
 istackRot :: IStack -> Maybe IStack
-istackRot = undefined
+istackRot (a : b : c : rest) = Just (c : a : b : rest)
+istackRot _ = Nothing
 
 --- ### Popping the Stack
 
 printPop :: ForthState -> ForthState
-printPop (i:istack, dict, out) =
-    (istack, dict, show i : out)
+printPop (i : istack, dict, out) =
+  (istack, dict, show i : out)
 printPop _ = underflow
 
 --- ### Printing the Stack
 
 printStack :: ForthState -> ForthState
-printStack (istack, dict, out) = undefined
+printStack (istack, dict, out) = (istack, dict, unwords (map show (reverse istack)) : out)
 
 --- Evaluator
 --- ---------
 
 eval :: [String] -> ForthState -> ForthState
-
 -- empty input -> return current state and output "ok"
-eval [] (istack, dict, out) = (istack, dict, "ok":out)
-
+eval [] (istack, dict, out) = (istack, dict, "ok" : out)
 --- ### Lookup in dictionary
 
 -- otherwise it should be handled by `dlookup` to see if it's a `Num`, `Prim`,
 -- `Define`, `Compile`, or `Unknown`
-eval (w:ws) state@(istack, dict, out)
-    = case dlookup w dict of
-        Prim f    -> eval ws (f state)
-        Num i     -> eval ws (i:istack, dict, out)
-        Define    ->
-            case compileDef ws dict of
-                Right (rest, dict') -> eval rest (istack, dict', out)
-                Left msg -> ([], dict, msg:out)
-        otherwise -> -- reset IStack and add error message
-            ([], dict, (show otherwise):out)
+eval (w : ws) state@(istack, dict, out) =
+  case dlookup w dict of
+    Prim f -> eval ws (f state)
+    Num i -> eval ws (i : istack, dict, out)
+    Define ->
+      case compileDef ws dict of
+        Right (rest, dict') -> eval rest (istack, dict', out)
+        Left msg -> ([], dict, msg : out)
+    otherwise ->
+      -- reset IStack and add error message
+      ([], dict, (show otherwise) : out)
 
 --- Compiler
 --- --------
 
 --- ### Definite Loops
 transForLoop :: Transition -> (ForthState -> ForthState)
-transForLoop kloop (i:is, d, o) =
-    if i < 0 then
-        (is, d, o)
+transForLoop kloop (i : is, d, o) =
+  if i < 0
+    then
+      (is, d, o)
     else
-        (aux kloop i) (is, d, o)
-        where aux k 0 = k
-              aux k n = k . (aux k (n-1))
+      (aux kloop i) (is, d, o)
+  where
+    aux k 0 = k
+    aux k n = k . (aux k (n - 1))
 transForLoop _ _ = underflow
 
-
 cstackFor :: CStack -> Maybe CStack
-cstackFor cstack = Just $ ("for", id):cstack
+cstackFor cstack = Just $ ("for", id) : cstack
 
 cstackNext :: CStack -> Maybe CStack
-cstackNext (("for", kloop):(c, kold):cstack) =
-    Just ((c, knew):cstack) where knew = (transForLoop kloop) . kold
+cstackNext (("for", kloop) : (c, kold) : cstack) =
+  Just ((c, knew) : cstack)
+  where
+    knew = (transForLoop kloop) . kold
 cstackNext _ = Nothing
 
 --- ### Conditionals
 
+transIf :: Transition -> Transition -> ForthState -> ForthState
+transIf kthen kelse (i : is, d, o)
+  | i /= 0 = kthen (is, d, o)
+  | otherwise = kelse (is, d, o)
+transIf _ _ _ = underflow
+
 cstackIf :: CStack -> Maybe CStack
-cstackIf cstack = undefined
+cstackIf cstack = Just $ ("if", id) : cstack
 
 cstackElse :: CStack -> Maybe CStack
-cstackElse cstack@(("if", _):_) = undefined
+cstackElse (("if", kif) : rest) = Just $ ("else", id) : ("if", kif) : rest
 cstackElse _ = Nothing
 
 cstackThen :: CStack -> Maybe CStack
-cstackThen (("else", kelse):("if", kif):(c, kold):cstack) = undefined
-cstackThen (("if", kif):(c, kold):cstack) = undefined
+cstackThen (("else", kelse) : ("if", kif) : (c, kold) : cstack) =
+  Just ((c, transIf kif kelse . kold) : cstack)
+cstackThen (("if", kif) : (c, kold) : cstack) =
+  Just ((c, transIf kif id . kold) : cstack)
 cstackThen _ = Nothing
 
 --- ### Indefinite Loops
 
+transUntilLoop :: Transition -> ForthState -> ForthState
+transUntilLoop kloop state =
+  case kloop state of
+    (i : is, d, o) ->
+      if i /= 0
+        then (is, d, o)
+        else transUntilLoop kloop (is, d, o)
+    _ -> underflow
+
 cstackBegin :: CStack -> Maybe CStack
-cstackBegin cstack = Just $ ("begin", id):cstack
+cstackBegin cstack = Just $ ("begin", id) : cstack
 
 cstackUntil :: CStack -> Maybe CStack
-cstackUntil (("begin", kloop):(c, kold):cstack) = undefined
+cstackUntil (("begin", kloop) : (c, kold) : cstack) =
+  Just ((c, transUntilLoop kloop . kold) : cstack)
 cstackUntil _ = Nothing
-
 
 --- ### Lookup in dictionary
 
@@ -240,30 +292,32 @@ cstackUntil _ = Nothing
 -- Assuming ':' is already stripped away
 compileDef :: [String] -> Dictionary -> Either ErrorMsg ([String], Dictionary)
 compileDef [] _ = Left msgZeroLenDef
-compileDef (name:ws) dict
-    = case  compile ws dict [("", id)] of
-       Right (rest, f) -> Right (rest, dinsert name (Prim f) dict)
-       Left msg -> Left msg
+compileDef (name : ws) dict =
+  case compile ws dict [("", id)] of
+    Right (rest, f) -> Right (rest, dinsert name (Prim f) dict)
+    Left msg -> Left msg
 
-compile :: [String] -> Dictionary -> CStack
-           -> Either ErrorMsg ([String], Transition)
+compile ::
+  [String] ->
+  Dictionary ->
+  CStack ->
+  Either ErrorMsg ([String], Transition)
 compile [] _ _ = Left "The definition does not end"
-
-compile (w:ws) dict cstack
-    = case dlookup w dict of
-        Prim f    -> compile ws dict (updateTop f cstack)
-        Num i     -> compile ws dict (updateTop f cstack)
-                     where f = (liftIStackOp (\is -> Just (i:is)))
-        Compile cf-> case cf cstack of
-                Just cstack' -> compile ws dict cstack'
-                Nothing -> Left $ msgUnstructured w
-        Define    -> Left "Nested definition is not allowed"
-        EndDef    -> case cstack of
-            [("", k)] -> Right (ws, k)
-            otherwise -> Left $ msgUnstructured w
-        otherwise -> Left $ show otherwise
+compile (w : ws) dict cstack =
+  case dlookup w dict of
+    Prim f -> compile ws dict (updateTop f cstack)
+    Num i -> compile ws dict (updateTop f cstack)
+      where
+        f = (liftIStackOp (\is -> Just (i : is)))
+    Compile cf -> case cf cstack of
+      Just cstack' -> compile ws dict cstack'
+      Nothing -> Left $ msgUnstructured w
+    Define -> Left "Nested definition is not allowed"
+    EndDef -> case cstack of
+      [("", k)] -> Right (ws, k)
+      otherwise -> Left $ msgUnstructured w
+    otherwise -> Left $ show otherwise
 
 updateTop :: Transition -> CStack -> CStack
-updateTop k ((c, kold):cs) = (c, k . kold) : cs
+updateTop k ((c, kold) : cs) = (c, k . kold) : cs
 updateTop _ [] = underflow
-
