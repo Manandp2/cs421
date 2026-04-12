88053dc6a39182ffd91e240df3d84f436c75b0b0
Mattox Beckman <mattox@illinois.edu>
2026-04-12 16:01:38 -0500

    Release mp5
    # Conflicts:
    #	mp5-scheme/src/Scheme/Eval.hs
    #	mp5-scheme/src/Scheme/Runtime.hs

--- mp5-scheme/src/Scheme/Runtime.hs
+++ mp5-scheme/src/Scheme/Runtime.hs
@@ -146,9 +146,10 @@ isList :: [Val] -> EvalState Val
 isList = const $ unimplemented "Primitive function `list?`"
 
 -- Primitive function `symbol?` predicate
--- TODO
 isSymbol :: [Val] -> EvalState Val
-isSymbol = const $ unimplemented "Primitive function `symbol?`"
+isSymbol [Symbol x] = return $ Boolean True
+isSymbol [_] = return $ Boolean False
+isSymbol e =  throwError $ UnexpectedArgs e
 
 -- Primitive function `pair?` predicate
 -- TODO
